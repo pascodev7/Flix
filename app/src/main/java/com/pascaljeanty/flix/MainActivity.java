50dcdafb09fc7c97cdb868b4c17292675a91cbe4
Pascal Jeanty <jeantypascal@yahoo.fr>
2022-07-20 05:31:10 -0400

    Commit networking setup and json parsing

--- app/src/main/java/com/pascaljeanty/flix/MainActivity.java
+++ app/src/main/java/com/pascaljeanty/flix/MainActivity.java
@@ -3,12 +3,50 @@ package com.pascaljeanty.flix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON Json) {
+                        // called when response HTTP status is "200 OK"
+                        Log.d(TAG, "onSuccess");
+                        JSONObject jsonObject = Json.jsonObject;
+                        try {
+                            JSONArray results = jsonObject.getJSONArray("results");
+                            Log.i(TAG, "Results: " + results.toString());
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Hit Json exception", e);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String errorResponse, Throwable throwable) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                        Log.d(TAG, "onFailure");
+                    }
+                }
+        );
     }
+
 }
