2ccbdedbe971c12a95ae98cc9b7844e30401fdb6
Pascal Jeanty <jeantypascal@yahoo.fr>
2022-07-24 22:51:41 -0400

    Commit changes made to display images and views on the phone screen

--- app/src/main/java/com/pascaljeanty/flix/MainActivity.java
+++ app/src/main/java/com/pascaljeanty/flix/MainActivity.java
@@ -1,16 +1,23 @@
 package com.pascaljeanty.flix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.pascaljeanty.flix.adapters.MovieAdapter;
+import com.pascaljeanty.flix.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -19,13 +26,25 @@ public class MainActivity extends AppCompatActivity {
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
 
+    List<Movie> movies;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+
+        // Create the adapter
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        //Set the adapter on the recycler view
+        rvMovies.setAdapter(movieAdapter);
+
+        //Set a layout manager on the recycler view
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         AsyncHttpClient client = new AsyncHttpClient();
-
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Headers headers, JSON Json) {
@@ -35,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             JSONArray results = jsonObject.getJSONArray("results");
                             Log.i(TAG, "Results: " + results.toString());
+                            movies.addAll(Movie.fromJsonArray(results));
+                            movieAdapter.notifyDataSetChanged();
+                            Log.i(TAG, "Movies: " + movies.size());
                         } catch (JSONException e) {
                             Log.e(TAG, "Hit Json exception", e);
                         }
